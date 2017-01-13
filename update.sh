6047f93e3691a860df8addd07fe049de32857ec3
Braxton <braxton.mclean5@gmail.com>
2017-01-13 07:03:24 -0500

    Fixed update
    Now specifically kills the old python process, instead of killing... all
    python processes.

--- update.sh
+++ update.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 cd ..
 sudo rm -r PantherBot-old
+pkill -f PantherBot/bot.py
 mv PantherBot PantherBot-old
 git clone https://github.com/braxton-mclean/PantherBot.git
 mv PantherBot-old/config PantherBot/config
 mv PantherBot-old/secrets PantherBot/secrets
 mv PantherBot-old/logs PantherBot/logs
-sudo reboot now
-pkill python
 cd PantherBot
 ./start.sh
