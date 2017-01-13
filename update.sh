94fe4a82140229850d206387f5cc58c6dd9535f8
Braxton McLean <braxton.mclean5@gmail.com>
2017-01-13 01:36:38 -0500

    small commit plz ignore

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ git clone https://github.com/braxton-mclean/PantherBot.git
 mv PantherBot-old/config PantherBot/config
 mv PantherBot-old/secrets PantherBot/secrets
 mv PantherBot-old/logs PantherBot/logs
+sudo reboot now
 pkill python
-cd .
 cd PantherBot
 ./start.sh
