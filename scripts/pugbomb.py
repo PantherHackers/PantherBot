82f15d7e968a6aeab034aad0a490cfa8ef87f0f8
HarshaGoli <harshagoli@gmail.com>
2017-02-09 17:35:15 -0500

    Fixed intent problems

--- scripts/pugbomb.py
+++ scripts/pugbomb.py
@@ -5,18 +5,18 @@ import praw
 
 #pug bombs the chat and destroys this poor bot's soul
 def pugbomb(response, args):
+    #gets the number
+    #num = [int(s) for s in response["text"].split() if s.isdigit()]
+    num = int(args[0])
+    if num > 10:
+        num = 10
 
-	#gets the number
-	#num = [int(s) for s in response["text"].split() if s.isdigit()]
-	num = int(args[0])
-	if num > 10:
-		num = 10
-
-	reddit = praw.Reddit(client_id='aGpQJujCarDHWA',
-                     	client_secret='fkA9lp0NDx23B_qdFezTeGyGKu8',
-                     	user_agent='my user agent',
-                        password='PHGSU2017',
-                        username='Panther_Bot')
+    reddit = praw.Reddit(client_id='aGpQJujCarDHWA',
+        client_secret='fkA9lp0NDx23B_qdFezTeGyGKu8',
+        user_agent='my user agent',
+        password='PHGSU2017',
+        username='Panther_Bot')
+    
     pug_urls=[]
     for submission in reddit.subreddit('pugs').hot(limit=num):
         pug_urls.append(submission.url)
