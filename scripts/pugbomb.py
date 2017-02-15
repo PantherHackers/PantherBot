35df4eb096cb0e0aab9adce9f7e425d5d4c44c95
HarshaGoli <harshagoli@gmail.com>
2017-02-15 16:06:16 -0500

    Added regex to add .jpg to imgur links that aren't .jpg or .png by default

--- scripts/pugbomb.py
+++ scripts/pugbomb.py
@@ -2,22 +2,30 @@
 # -*- coding: utf-8 -*-
 #Created by Harsha Goli
 import praw
+import re
 
 #pug bombs the chat and destroys this poor bot's soul
 def pugbomb(response, args):
     #gets the number
-    #num = [int(s) for s in response["text"].split() if s.isdigit()]
     num = int(args[0])
     if num > 10:
         num = 10
 
     reddit = praw.Reddit(client_id='aGpQJujCarDHWA',
-        client_secret='fkA9lp0NDx23B_qdFezTeGyGKu8',
-        user_agent='my user agent',
-        password='PHGSU2017',
-        username='Panther_Bot')
+                        client_secret='fkA9lp0NDx23B_qdFezTeGyGKu8',
+                        user_agent='my user agent',
+                        password='PHGSU2017',
+                        username='Panther_Bot')
     
     pug_urls=[]
     for submission in reddit.subreddit('pugs').hot(limit=num):
-        pug_urls.append(submission.url)
-	return pug_urls
+        url = submission.url
+        if re.search(r'(imgur)', submission.url, flags = 0) != None and re.search(r'(\.jpg|\.png|\.jpeg)', submission.url, flags = 0) == None:
+            url+=('.jpg')
+        
+        pug_urls.append(url)
+
+    pug_urls.append("""
+        `Having issues? Try Preferences > Messages > 'Even if theyre larger than 2MB'`
+        """)  
+    return pug_urls
