20c82b9ac6acf08b98a5cf1bacd226303fc712d2
Phillip Kinney <--global>
2017-03-25 02:42:44 -0400

    Last mistake fixed hopefully

--- scripts/helloworld.py
+++ scripts/helloworld.py
@@ -6,7 +6,7 @@
 # Name your file with the title of your method, both of which should be all lowercase.
 def helloworld(response, args=None):  # response is always given to you, good for checking on user info or something unique to a message object, args is optional, or if your function may not take args, set it to None or [] depending on your needs
     # do your logic here
-    message = "Hello World: "
+    message = "Hello World:"
     if args is not None:
         for x in range(0, len(args)):  # For loop that goes from the second element of args (to skip the command) to the last element
             message += " " + args[x]  # adds a space and the next argument from the message call
