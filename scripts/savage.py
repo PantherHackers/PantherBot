2776ac3fd3c20af0ce89e4bd19abd2a159d7e252
Nishant Sinha <nishnha@gmail.com>
2017-04-12 20:35:22 -0400

    Added !savage command for @Jim

---
+++ scripts/savage.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def savage(response):
+    savage_gif = "https://slack-imgs.com/?c=1&url=https%3A%2F%2Fi.giphy.com%2F11LAY5CMZsfMYg.gif"
+
+    return savage_gif
