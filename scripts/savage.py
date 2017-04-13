5c2ff4dd64b6afca56c5c81e17f7448b0332425e
Nishant Sinha <nishnha@gmail.com>
2017-04-12 20:43:19 -0400

    Changed the web host of the gif to imgur

--- scripts/savage.py
+++ scripts/savage.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 
 def savage(response):
-    savage_gif = "https://slack-imgs.com/?c=1&url=https%3A%2F%2Fi.giphy.com%2F11LAY5CMZsfMYg.gif"
+    savage_gif = "http://i.imgur.com/aE41Aoo.gif"
 
     return savage_gif
