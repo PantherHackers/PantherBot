9f55719f274a03ca01a0bc214e8a4e689148e28f
Nate Harris <n8gr8gbln@gmail.com>
2019-09-05 18:42:08 -0400

    Added args to links.py, get specific link

--- scripts/links.py
+++ scripts/links.py
@@ -8,7 +8,7 @@ from pb_logging import PBLogger
 logger = PBLogger("Links")
 
 # flips a coin
-def run(response):
+def run(response, args):
     response_obj = Response(sys.modules[__name__])
     res_message = ""
     links = {
@@ -18,8 +18,21 @@ def run(response):
         "Twitter":"twitter.com/pantherhackers",
         "GitHub":"github.com/PantherHackers"
     }
-    for n, l in links:
-        res_message = res_message + n + ": " + l + "\n"
+    specific_link = False
+    if args:
+        specific_link = True
+    if specific_link:
+        try:
+            for a in args:
+                res_message = res_message + a.lower().capitalize() + ": " + links[a.lower().capitalize()] + "\n"
+        except Exception as e:
+            logger.error(a.lower().capitalize() + " not found in links")
+            specific_link = False
+            break
+    if not specific_link:
+        res_message = ""
+        for n, l in links:
+            res_message = res_message + n + ": " + l + "\n"
     response_obj.messages_to_send.append(res_message)
     logger.info(res_message)
     return response_obj
@@ -32,7 +45,7 @@ def is_admin_command():
     return False
 
 def help_preview():
-    return "!links"
+    return "!links <Optional:String>"
 
 def help_text():
     return "Lists links to revelant pages and social accounts."
